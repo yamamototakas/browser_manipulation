13996dae04a6f39614a6f2c2ecb91e8ee4352708
pêtty <ya.ma.mo.to.ta.ka.s@gmail.com>
2017-03-31 09:30:27 +0700

    Change how to call browser

--- click_site.py
+++ click_site.py
@@ -4,14 +4,15 @@ import random
 import re
 import sys
 import time
-from subprocess import CalledProcessError, check_call
+from subprocess import Popen
 
 import browser_cookie3
 import requests
 
 HEADERS = {"Connection": "keep-alive",
            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
-           "User-Agent": "Mozilla/5.0 (Windows NT 6.0) AppleWebKit/535.2 (KHTML, like Gecko) Chrome/15.0.874.120 Safari/535.2",
+           "User-Agent": "Mozilla/5.0 (Windows NT 6.0) AppleWebKit/535.2 (KHTML, like Gecko) \
+                          Chrome/15.0.874.120 Safari/535.2",
            "Referer": "http://hapitas.jp/",
            "Accept-Encoding": "gzip,deflate,sdch",
            "Accept-Language": "ja,en-US;q=0.8,en;q=0.6"
@@ -31,10 +32,10 @@ def main():
     print(URL_LIST)
 
     req_session = requests.session()
-    #cj = browser_cookie3.load()
+    # cj = browser_cookie3.load()
     cj = browser_cookie3.firefox()
-    #cj = browser_cookie3.chrome()
-    #print (cj)
+    # cj = browser_cookie3.chrome()
+    # print (cj)
 
     for each_url in URL_LIST:
         print(each_url)
@@ -63,7 +64,7 @@ def main():
             # http://hapitas.jp/item/redirect/itemid/52128/p/0/click/1634/apn/top_clickget
             # http://hapitas.jp/item/redirect/itemid/65202/p/0/click/2566/apn/top_clickget/linkid/856
 
-            #key2 = '(item\/|condition\/)(.+top_clickget.*)("\sclass|"\s>)'
+            # key2 = '(item\/|condition\/)(.+top_clickget.*)("\sclass|"\s>)'
             key2 = '(item\/)(.+top_clickget.*)("\sclass)'
             pattern2 = re.compile(key2)
             result2 = pattern2.findall(page2)
@@ -81,8 +82,7 @@ def main():
                     result3 = pattern3.findall(page3)
 
                     if result3:
-                        temp_url = 'http://hapitas.jp/' + \
-                            result3[0][0] + result3[0][1]
+                        temp_url = 'http://hapitas.jp/' + result3[0][0] + result3[0][1]
 
                 url2.append(temp_url)
 
@@ -93,25 +93,22 @@ def main():
             print("url =", each)
             try:
                 page4 = req_session.get(each, headers=HEADERS, cookies=cj)
-            except:
+            except Exception as err:
                 print("--------------------------------------------")
                 print(" Error happens in sending GET to the following page")
                 print("URL = ", each)
                 print("--------------------------------------------")
+                print("ERROR : ", sys.exc_info())
                 # print traceback.format_exc(sys.exc_info()[2])
             finally:
                 time.sleep(random.randint(1, 2) + random.randint(0, 1))
 
-    #check_call(["C:\Program Files (x86)\Google\Chrome\Application\chrome.exe", " --disable-images", "http://hapitas.jp/"])
-
     try:
-        check_call(
-            ["C:/Program Files (x86)/Mozilla Firefox/firefox.exe", " http://hapitas.jp/"])
+        proc = Popen(["C:/Program Files (x86)/Mozilla Firefox/firefox.exe", " http://hapitas.jp/"])
+        # check_call(
+        #     ["C:/Program Files (x86)/Mozilla Firefox/firefox.exe", " http://hapitas.jp/"])
     except CalledProcessError as err:
         pass
-    except Exception as err:
-        print("ERROR: ", sys.exc_info())
-
     print('ENDENDEND')
     time.sleep(5)
 
